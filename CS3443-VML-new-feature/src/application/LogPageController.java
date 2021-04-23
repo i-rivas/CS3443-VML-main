dc9e40b7cb014bdd864d03e910eb5f07f739dc58
Diego Zamora <diego072.lo@gmail.com>
2021-04-23 17:58:26 -0500

    Pull Menu and LogPage into project

--- CS3443-VML-new-feature/src/application/LogPageController.java
+++ CS3443-VML-new-feature/src/application/LogPageController.java
@@ -1,19 +1,21 @@
 package application;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import java.util.*;
+import javafx.application.Application;
+import javafx.fxml.FXML;
 
 
-public class Main extends Application {
+public class LogPageController extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Menu.fxml"));
-			Scene scene = new Scene(root,600,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("LogPage.fxml"));
+			Scene scene = new Scene(root,536,322);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
