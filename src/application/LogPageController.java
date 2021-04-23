ab6e9f90147ed57aab8d013a71ea2f5a9c1ad628
James <james.le1776@gmail.com>
2021-04-23 18:57:26 -0500

    LogPage update

--- src/application/LogPageController.java
+++ src/application/LogPageController.java
@@ -1,16 +1,22 @@
 package application;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.*;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 
 public class LogPageController extends Application{
+	private AnchorPane mainPane;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -27,4 +33,13 @@ public class LogPageController extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	@FXML
+    public void switch2Main(ActionEvent event) throws IOException {
+        mainPane = FXMLLoader.load(getClass().getResource("Menu.fxml"));// pane you are GOING TO
+        Scene scene = new Scene(mainPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+    }
 }
