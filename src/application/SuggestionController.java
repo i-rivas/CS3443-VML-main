6deb7ed8c9a25bdf7f23fdc879da703ed2c4fc27
James <james.le1776@gmail.com>
2021-04-23 19:28:31 -0500

    New feature Vehicle Suggestion

---
+++ src/application/SuggestionController.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SuggestionController {
+	AnchorPane mainPane;
+	
+	@FXML
+    public void goToList(ActionEvent event) throws IOException {
+		mainPane = FXMLLoader.load(getClass().getResource("VehicleList.fxml"));// pane you are GOING TO
+        Scene scene = new Scene(mainPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+    }
+	
+	@FXML
+    public void switch2Main(ActionEvent event) throws IOException {
+        mainPane = FXMLLoader.load(getClass().getResource("Menu.fxml"));// pane you are GOING TO
+        Scene scene = new Scene(mainPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+    }
+}
