23c0fb5183f659d5ad74df1b1d94367f337360d3
Diego Zamora <diego072.lo@gmail.com>
2021-04-23 19:02:20 -0500

    Can switch scenes between Menu, LogPage, and VehicleList

--- CS3443-VML-new-feature/src/application/LogPageController.java
+++ CS3443-VML-new-feature/src/application/LogPageController.java
@@ -1,30 +1,26 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import java.util.*;
-import javafx.application.Application;
-import javafx.fxml.FXML;
+import model.Model;
 
-
-public class LogPageController extends Application{
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("LogPage.fxml"));
-			Scene scene = new Scene(root,536,322);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class LogPageController {
+	@FXML
+    private AnchorPane mainPane;
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	@FXML
+    public void switch2Main(ActionEvent event) throws IOException {
+        mainPane = FXMLLoader.load(getClass().getResource("Menu.fxml"));// pane you are GOING TO
+        Scene scene = new Scene(mainPane);// pane you are GOING TO show
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
+        window.setScene(scene);
+        window.show();
+    }
 }
