855fc84ee1b2bc20f27c73e5736ebd5f25cee823
jam3264 <79064627+jam3264@users.noreply.github.com>
2021-04-28 17:45:30 -0500

    Update Model.java

--- src/application/Model.java
+++ src/application/Model.java
@@ -20,7 +20,7 @@ import javafx.scene.control.ListView;
 public class Model {
 	
 	private static ArrayList<String> items = new ArrayList<String>();
-	
+	public static ArrayList<Vehicle> listofVehicles = new ArrayList<Vehicle>();
 	public static ArrayList<String> listofVehicles = new ArrayList<String>();
 	
 	public static void getVehicleList(ListView<String> printList) throws FileNotFoundException {
@@ -75,61 +75,112 @@ public class Model {
         read.close();
 	}
 	
-	public static boolean getVehicle(String vehicleKey) throws FileNotFoundException, IOException {
-		Boolean inList = false;
-		String foundVeh = null;
-		String lastDate = null;
-		String vehIssue = null;
-    	File inFile = new File("VehicleList.txt");
+    public static Vehicle getVehicle(String vehicleKey) throws FileNotFoundException, IOException 
+    {
+    	Boolean inList = false;
+    	File inFile = new File("VehicleList2.txt");
 		Scanner read = new Scanner(inFile);
-    	
+		String[] arr;
+    	ArrayList<String> textList = new ArrayList<String>();
     	while (read.hasNextLine()) {
-			listofVehicles.add(read.nextLine());
+			textList.add(read.nextLine());
     	}
     	read.close();
-    	
-    	for(String veh: listofVehicles)
+
+    	for(String line: textList)
     	{
-    		
-    		if( veh.contains(vehicleKey) )
+    		// Split 1 line of text into 3 parts, vehicleName at arr[0], date at arr[1], issue at arr[2]
+    		arr = line.split("	");
+    		Vehicle newVehicle = new Vehicle(arr[0], arr[1], arr[2]);
+    		listofVehicles.add(newVehicle);
+    	}
+    	
+    	for(Vehicle veh: listofVehicles)
+    	{
+    		if( vehicleKey.equals(veh.getVehicleMake()) )
     		{
     			inList = true;
-    			/*
-    			int startIndex = veh.indexOf(vehicleKey);
-    			int endIndex = startIndex + vehicleKey.length();
-    			foundVeh = veh.substring(startIndex, endIndex);
-    			*/
-    			// Create a Pattern object
-    			String pattern = "(.*)[ \\t]+([0-9]{2}/[0-9]{2}/[0-9]{4})[ \\t]+(.*)";
-    			Pattern r = Pattern.compile(pattern);
-
-    			// Now create matcher object.
-    			Matcher m = r.matcher(veh);
-    			if (m.find( )) {
-    				System.out.println("Found value: " + m.group(0) );
-    				System.out.println("Found value: " + m.group(1) );
-    				System.out.println("Found value: " + m.group(2) );
-    			}else {
-    				System.out.println("NO MATCH");
-    			}
+    			return veh;
     		}
-    				
     	}
     	if( inList == false )
     	{
-    		Alert a = new Alert(AlertType.NONE);
-    		// set alert type
-    		a.setAlertType(AlertType.ERROR); 
-    		// show the dialog 
-    		a.show();
-    		a.setContentText("Vehicle does not exist in List");
-    	}    	
-    	System.out.println(foundVeh);
-		return true;
-	}
+    		return null;
+    	}
+    	
+    	/*
+    	for(Vehicle veh: listofVehicles)
+    	{
+    		System.out.println("Vehicle Make: " + veh.getVehicleMake());
+    		System.out.println("Date: " + veh.getlastCheckedDate());
+    		System.out.println("Issue: " + veh.getCurrentIssue());
+    	}
+    	*/
+		return null;
+    }
 	
-	public static void checkVehicle(String vehicleName, String date, String issue)
+	public static String[] checkVehicle(String vehicleName, String date, String issue)
 	{
-		
+		String[] messages = new String[5];
+		int i = 0;
+		// Date format: mm/dd/yyyy
+		 // Get an instance of LocalTime
+        // from date
+        LocalDate currentDate = LocalDate.now();
+        // Get day from date
+        int day = currentDate.getDayOfMonth();
+        // Get month from date
+        Month month = currentDate.getMonth();
+        int todaymonth = month.getValue();
+        // Get year from date
+        int year = currentDate.getYear();
+  
+        // Print the day, month, and year
+        System.out.println("Day: " + day);
+        System.out.println("Month: " + month);
+        System.out.println("Year: " + year);
+		int yeardiff = 2021 - Integer.parseInt(date.substring(6));
+		int monthdiff = todaymonth - Integer.parseInt(date.substring(0,2));
+		System.out.println("month substring = " + date.substring(0,2) );
+		System.out.println("monthdiff = " + monthdiff);
+		System.out.println("monthval = " + todaymonth);
+		//int daydiff;
+		if( yeardiff > 0 )
+		{
+			messages[i] = "It has been " + yeardiff + " years since your last maintenance\n";
+			i++;
+		}
+		else if ( monthdiff > 6 )
+		{
+			messages[i] = "It has been " + monthdiff + " months since your last maintenance\n";
+			i++;
+		}
+		else 
+		{
+			messages[i] = "Your last maintenance was already done within the last 6 months\n";
+			i++;
+		}
+	
+		if( issue.equals("Engine Repair") )
+		{
+			messages[i] = "You need a engine repair\n";
+			i++;
+		}
+		if( issue.equals("Vehicle Maintenance") )
+		{
+			messages[i] = "You need vehicle maintenance\n";
+			i++;
+		}
+		if( issue.equals("Oil Leak Repair") )
+		{
+			messages[i] = "You need an oil leak repaired\n";
+			i++;
+		}
+		if( issue.equals("Broken Windowshield") )
+		{
+			messages[i] = "You need a windowshield replacement\n";
+			i++;
+		}
+		return messages;
 	}
 }
