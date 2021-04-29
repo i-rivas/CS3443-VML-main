1d73c64038a1cf5f4eddc7960af76e9f0b76e9fb
jam3264 <79064627+jam3264@users.noreply.github.com>
2021-04-28 20:53:16 -0500

    New feature, schedule appointment and export to txt

--- src/application/SuggestionController.java
+++ src/application/SuggestionController.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import model.Model;
-import model.Vehicle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,13 +14,23 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.Model;
+import model.Vehicle;
 
 public class SuggestionController {
 	AnchorPane mainPane;
+	
+	@FXML
+    private TextField timeField;
+	
+	@FXML
+	private TextField repairShop;
 	
 	@FXML
     private TextField searchBar;
@@ -30,6 +43,12 @@ public class SuggestionController {
     	searchBar.clear();
     	message.clear();
     }
+	
+	@FXML
+    private Button exportButton;
+	
+	 @FXML
+    private DatePicker appointDatePicker;
 	
 	@FXML
     public void checkVehicleList(ActionEvent event) throws IOException {
@@ -66,6 +85,45 @@ public class SuggestionController {
     		message.appendText(newText[i]);
     	}
 	}
+	
+	//Export method
+	@FXML
+	private void exportAppointment(ActionEvent event) throws IOException {
+		//Note for later: Get the vehicle name from search bar!!!
+		LocalDate date = appointDatePicker.getValue();
+		String formattedDate = date.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+		String shop = repairShop.getText();
+		String time = timeField.getText();
+		String appointment = formattedDate + ", " + time + ", " + shop;
+		try {
+			File file = new File("Schedule.txt");
+			FileWriter myWriter = new FileWriter(file, true);
+			BufferedWriter bufferWrite = new BufferedWriter(myWriter);
+			bufferWrite.write(appointment);
+			bufferWrite.newLine();
+			bufferWrite.close();
+			myWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+    public void clearFields(ActionEvent event) {
+		appointDatePicker.setValue(null);
+		timeField.clear();
+		repairShop.clear();
+    }
+	
+    @FXML
+    public void createAppointment(ActionEvent event) {
+    	Alert a = new Alert(AlertType.NONE);
+    	// set alert type
+    	a.setAlertType(AlertType.CONFIRMATION); 
+    	// show the dialog 
+    	a.show();
+    	a.setContentText("Do you want to schedule the appointment?");
+    }
 	
 	@FXML
     public void goToList(ActionEvent event) throws IOException {
