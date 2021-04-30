08fdea2c200f40132880dfde3f164c67f90ac78d
zDiego27 <80714969+zDiego27@users.noreply.github.com>
2021-04-29 20:37:13 -0500

    Update Menu.fxml and MenuControl.java

--- src/application/MenuControl.java
+++ src/application/MenuControl.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ResourceBundle;
+
+import javax.print.DocFlavor.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class MenuControl {
